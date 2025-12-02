dbac8be42d84c367546dc43aecdc5a4281359171
Alexandre Payen <alexandre@grauw.fr>
2025-12-02 19:15:40 +0100

    feat: generated better types, removing the default 'children' from svelte/element type. big thanks to bitsui for the id

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,67 +1,67 @@
-import type {RemoteFormField, RemoteFormInput, RemoteFormIssue, RemoteQuery, RemoteQueryOverride} from "@sveltejs/kit";
-import type {StandardSchemaV1} from "@standard-schema/spec";
-import type {Snippet} from "svelte";
-import type {HTMLAttributes, HTMLFieldsetAttributes} from "svelte/elements";
+import type {
+	RemoteFormField,
+	RemoteFormInput,
+	RemoteFormIssue,
+	RemoteQuery,
+	RemoteQueryOverride
+} from '@sveltejs/kit';
+import type { StandardSchemaV1 } from '@standard-schema/spec';
+import type { Snippet } from 'svelte';
+import type { HTMLAttributes, HTMLFieldsetAttributes } from 'svelte/elements';
 
-import type {FaurmContext} from "./use-faurm.svelte.js";
+type FaurmPrimitive<T> = Omit<T, 'style' | 'id' | 'children'> & { id?: string };
 
 export type UseFaurmOpts<RFInput extends RemoteFormInput> = {
-    initialData?: Partial<RFInput>
-    validate?: StandardSchemaV1<RFInput>,
-    resetForm?: boolean,
+	initialData?: Partial<RFInput>;
+	validate?: StandardSchemaV1<RFInput>;
+	resetForm?: boolean;
 
-    delay?: number,
-    timeout?: number
+	delay?: number;
+	timeout?: number;
 
-    onSubmit?: (data: RFInput) => void
-    onSuccess?: () => void
-    onValidationError?: () => void,
-    updates?: () => Array<RemoteQuery<any> | RemoteQueryOverride>
-}
-
+	onSubmit?: (data: RFInput) => void;
+	onSuccess?: () => void;
+	onValidationError?: () => void;
+	updates?: () => Array<RemoteQuery<any> | RemoteQueryOverride>;
+};
 
 export type WithChild<T extends unknown[]> = {
-    child?: Snippet<T>
-}
+	child?: Snippet<T>;
+};
 
 export type FieldProps = {
-    field: RemoteFormField<any>
-    id?: string,
-    children?: Snippet
-}
+	field: RemoteFormField<any>;
+	id?: string;
+	children?: Snippet;
+};
 
-
-export type FieldSetProps =
-    Omit<HTMLFieldsetAttributes, 'form'>
-    & FieldProps
-    & WithChild<[{ props: HTMLFieldsetAttributes }]>
-    & {
-    children: Snippet,
-}
+export type FieldSetProps = Omit<HTMLFieldsetAttributes, 'form'> &
+	FieldProps &
+	WithChild<[{ props: HTMLFieldsetAttributes }]> & {
+		children: Snippet;
+	};
 
 export type FieldControlProps<TField> = {
-    children: Snippet<[{ props: any, field: TField }]>
-}
+	children: Snippet<[{ props: any; field: TField }]>;
+};
 
-export type LegendProps =
-    HTMLAttributes<HTMLLegendElement>
-    & WithChild<[{ props: HTMLAttributes<HTMLLegendElement> }]>
-    & {
-    children?: Snippet,
-}
+export type LegendProps = FaurmPrimitive<HTMLAttributes<HTMLLegendElement>> &
+	WithChild<[{ props: HTMLAttributes<HTMLLegendElement> }]> & {
+		children?: Snippet;
+	};
 
-export type LabelProps = HTMLAttributes<HTMLLabelElement>
-    & WithChild<[{ props: HTMLAttributes<HTMLLabelElement> }]>
-    & {
-    children?: Snippet
-}
+export type LabelProps = FaurmPrimitive<HTMLAttributes<HTMLLabelElement>> &
+	WithChild<[{ props: HTMLAttributes<HTMLLabelElement> }]> & {
+		children?: Snippet;
+	};
 
-export type DescriptionProps = HTMLAttributes<HTMLElement>
-    & WithChild<[{ props: HTMLAttributes<HTMLElement> }]>
-    & {
-    children?: Snippet
-}
+export type DescriptionProps = FaurmPrimitive<HTMLAttributes<HTMLElement>> &
+	WithChild<[{ props: HTMLAttributes<HTMLElement> }]> & {
+		children?: Snippet;
+	};
 
-export type FieldErrorsProps = HTMLAttributes<HTMLElement> & {
-    children?: Snippet<[{ issues: RemoteFormIssue[] | undefined, props: HTMLAttributes<HTMLElement> }]>
-}
+export type FieldErrorsProps = FaurmPrimitive<HTMLAttributes<HTMLElement>> & {
+	children?: Snippet<
+		[{ issues: RemoteFormIssue[] | undefined; props: HTMLAttributes<HTMLElement> }]
+	>;
+};
